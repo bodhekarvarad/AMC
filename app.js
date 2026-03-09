67a04ca969e1f38584f56be653d83cea2b9ccc0d
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-03-09 17:47:19 +0530

    adding ejsMate for boilerplate.ejs

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const ejsMate = require('ejs-mate');
 const app = express();
-const port=8500;
+const port=3000;
+//middlewares
+app.engine('ejs',ejsMate);
+app.set('view engine','ejs');
+app.set("views",path.join(__dirname,"views"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+
+//dashboard
 app.get('/',(req,res)=>{
     res.send('Dashboard is spaces');
 });
-app.get('/login',(req,res)=>{
-res.render('login');
-});
+
+
+
+
+
+
+
+
+//server starting
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
