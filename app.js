aec4e4763bd6a2e4c83ee886986859dd41eb82bb
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-03-11 17:31:01 +0530

    adding a middlerware and render navbar,footer & boilerplate in ejs format

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
 const express = require('express');
 const ejsMate = require('ejs-mate');
 const path = require("path");
+
 const app = express();
-const port=3000;
+const port = 3000;
+
 //middlewares
-app.engine('ejs',ejsMate);
-app.set('view engine','ejs');
-app.set("views",path.join(__dirname,"views"));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'public')));
 
 //dashboard
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Dashboard is spaces');
 });
-app.get('/navbar',(req,res)=>{
-    res.render('.../views/includes/navbar.ejs');
+
+app.get('/navbar', (req, res) => {
+    res.render('includes/navbar');
 });
 
-
-
-
-
-
-
-
 //server starting
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
